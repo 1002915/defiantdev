d6370cc18196884f78300e010b42df1ecc63aeaf
Fae Daunt <faedaunt@gmail.com>
2016-09-02 09:31:56 +1000

    Anchor - Batch 2
    Maybe this will help

---
+++ anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'url' => '/2016/anchor-cms',
+	'index' => '',
+	'timezone' => 'Australia/Brisbane',
+	'key' => 'caZouFygAiV6v3hxwljgNUwVJb3hoi4h',
+	'language' => 'en_GB',
+	'encoding' => 'UTF-8'
+);
---
+++ anchor/config/db.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'default' => 'mysql',
+	'prefix' => 'anchor_',
+	'connections' => array(
+		'mysql' => array(
+			'driver' => 'mysql',
+			'hostname' => 'localhost',
+			'port' => '3306',
+			'username' => 'root',
+			'password' => 'root',
+			'database' => 'defiant_cms',
+			'charset' => 'utf8'
+		)
+	)
+);
